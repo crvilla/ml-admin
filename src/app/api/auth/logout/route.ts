28824b3befd9c6b57cbccb7bb8498a8be16a5c3a
StiffMailo <stivenmaldonado@mailotech.com>
2025-07-29 20:43:07 -0500

    redirect

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function POST() {
-  const response = NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_APP_URL))
+  const response = NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_APP_URL))
 
   response.cookies.set({
     name: 'token',
